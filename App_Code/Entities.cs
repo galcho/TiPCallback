b6b3e39ca00dc52cae7e769735d0d34b8b0929d3
galcho <iliev@galcho.com>
2014-12-03 19:06:37 -0800

    Add barebone files needed for TiP callback

---
+++ App_Code/Entities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TipCallback.Models
+{
+    [DataContract]
+    public class MetricsWrapper
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember]
+        public object Activity { get; set; }
+    }
+
+    [DataContract]
+    public class Metrics
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        public SiteHttpActivity Activity { get; set; }
+    }
+
+
+    [DataContract]
+    public class ChangeDirectionResult
+    {
+        [DataMember(Name = "step")]
+        public int? Step { get; set; }
+
+        [DataMember(Name = "routingPercentage")]
+        public int? RoutingPercentage { get; set; }
+    }
+
+    [DataContract]
+    public class RerouteChangeRequest
+    {
+        [DataMember]
+        public Dictionary<string, SiteHttpActivity> Metrics { get; set; }
+    }
+
+    [DataContract]
+    public class SiteHttpActivity
+    {
+        [DataMember]
+        public long Requests { get; set; }
+    }
+}
---
+++ App_Code/RoutingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TipCallback.Models;
+
+namespace TipCallback.Controllers
+{
+    public class RoutingController : ApiController
+    {
+        [HttpPost]
+        public ChangeDirectionResult GetRoutingDirection([FromBody] RerouteChangeRequest metrics)
+        {
+            return new ChangeDirectionResult
+            {
+                Step = metrics.Metrics.Count * 8,
+                RoutingPercentage = 10
+            };
+        }
+
+        [HttpGet]
+        public ChangeDirectionResult Get()
+        {
+            return new ChangeDirectionResult
+            {
+                RoutingPercentage = 10
+            };
+        }
+    }
+}
